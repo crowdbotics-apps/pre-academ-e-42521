e955b9e0c4490ddfdc39da5b43f9a793f6545222
Crowdbotics <team@crowdbotics.com>
2023-07-30 08:39:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pre-academ-e-42521",
+  url: "https://pre-academ-e-42521.botics.co",
+  api: "https://pre-academ-e-42521.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
